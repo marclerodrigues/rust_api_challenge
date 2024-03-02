c1855847e9daf4f2c4a43f7578f7aa823eae133d
Marcle Rodrigues <maarclee@gmail.com>
2024-03-01 21:55:28 -0300

    Add logger

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 mod dto;
 mod handlers;
 
+use actix_web::middleware::Logger;
 use actix_web::{web, HttpResponse};
 use dto::Item;
+use env_logger::Env;
 use std::sync::Mutex;
 
 struct AppState {
@@ -39,8 +41,13 @@ async fn main() -> std::io::Result<()> {
 
     use actix_web::{App, HttpServer};
 
+    // Initialize logger
+    env_logger::init_from_env(Env::default().default_filter_or("info"));
+
     HttpServer::new(move || {
         App::new()
+            .wrap(Logger::default())
+            .wrap(Logger::new("%a %{User-Agent}i"))
             .app_data(shared_data.clone())
             .app_data(web::JsonConfig::default().error_handler(handlers::json_error_handler))
             .route("/items", web::post().to(create))
